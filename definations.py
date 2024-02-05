50a774a7ff6d04871ad8867745f9ed331f386add
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2024-02-05 21:43:34 +0200

    ../Definations

---
+++ definations.py
@@ -0,0 +1,10 @@
+# ----------------------Variables: 
+# Variables are containers for storing data values
+#    Variables in Python are used to store data values. 
+#    A variable is created the moment you first assign a value to it. Variable names in Python can contain letters, numbers, and underscores but cannot start with a number. They are case-sensitive.
+
+
+# ---------------------Functions: 
+# Functions are blocks of reusable code that perform a specific task. 
+# They provide modularity and help in code organization. 
+# In Python, you define a function using the def keyword followed by the function name, parameters (if any), and a colon. Functions can take inputs, perform operations, and optionally return a result using the return statement.
