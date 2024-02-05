c8b255f6bf89d95f0ffe3bb0ff4dec15785992a5
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2024-02-05 20:43:54 +0200

    ../

--- python.py
+++ python.py
@@ -1,2 +1,16 @@
 # The "print" function is used to display information
 print("Hello, World!")
+
+
+# Variables
+name = "John"
+age = 25
+height = 1.75
+
+# Data Types
+# String
+print("Name:", name)
+# Integer
+print("Age:", age)
+# Float
+print("Height:", height)
