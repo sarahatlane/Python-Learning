e8acf27694d744083914440cef8ab754ccbb2de8
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2024-02-05 21:46:02 +0200

    ../Definations

--- definations.py
+++ definations.py
@@ -7,4 +7,11 @@
 # ---------------------Functions: 
 # Functions are blocks of reusable code that perform a specific task. 
 # They provide modularity and help in code organization. 
-# In Python, you define a function using the def keyword followed by the function name, parameters (if any), and a colon. Functions can take inputs, perform operations, and optionally return a result using the return statement.
+# In Python, you define a function using the def keyword followed by the function name, parameters (if any), and a colon. 
+# Functions can take inputs, perform operations, and optionally return a result using the return statement.
+
+
+# ---------------------Lists:
+# Lists are ordered collections of items in Python. 
+# They are mutable, meaning their elements can be changed after creation. 
+# Lists are defined using square brackets [] and can contain elements of different data types, including other lists.
