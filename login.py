f33ae0a2114162ae22226be5ce578da0e6ca249f
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2024-02-05 21:17:48 +0200

    ../

---
+++ login.py
@@ -0,0 +1,4 @@
+# defining a dictionary called credentials where the keys are usernames and the values are passwords.
+# defining a function login() that prompts the user to enter their username and password using the input() function.
+# Inside the login() function, we check if the entered username exists in the credentials dictionary and if the entered password matches the password associated with that username.
+# If the username and password are correct, we print a success message. Otherwise, we print an error message.
